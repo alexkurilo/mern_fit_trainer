32396e2586955f6e67580167ba39780c368c33a4
Oleksii Kurilo <alex.kurilo77@gmail.com>
2020-03-20 16:51:48 +0200

    connect mongo and create server

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const app = express();
+const PORT = 5000;
+const mongoUri = config.get('mongoUri');
+
+app.use(express.json({ extended: true }));
+
+async function start () {
+    try {
+        await mongoose.connect(mongoUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+    } catch (e) {
+        console.log(`Server error`, e.message);
+        process.exit(1);// выйти из процесса
+    }
+}
+
+start();
