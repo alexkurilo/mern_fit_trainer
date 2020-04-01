ef8cf2f7d185c67adedaeee2654d2020bd6f59c7
Oleksii Kurilo <alex.kurilo77@gmail.com>
2020-04-01 17:21:49 +0300

    code style

--- client/src/reducers/socialNetworksReducer.js
+++ client/src/reducers/socialNetworksReducer.js
@@ -2,9 +2,6 @@ const initialState = [];
 
 export default function socialNetworks (state = initialState, action){
     switch (action.type) {
-        // case 'SAVE_SOC_NET':
-        //     return [...action.payload];
-
         case 'ADD_SOC_NET':
             state = state.includes(action.payload) ? state : state.concat(action.payload);
             return [...state];
